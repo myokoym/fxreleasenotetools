dd6461b25252e0aa26749d89c10584ae0df60159
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-12-08 20:38:27 +0900

    Add formatter for Markdown

---
+++ formatter.rb
@@ -0,0 +1,17 @@
+require "./parser"
+
+markdown = ""
+
+versions = Parser.new.parse
+versions.each do |version|
+  markdown << <<-MARKDOWN
+* [Firefox #{version.version} リリースノート](#{version.url})
+  MARKDOWN
+  version.changes.each do |change|
+  markdown << <<-MARKDOWN
+  * #{change.description}
+  MARKDOWN
+  end
+end
+
+puts markdown
